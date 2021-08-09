caa558135faf59ea17af5ee23830f0253b916ffb
seongbinko <wnrhd1082@gmail.com>
2021-08-09 11:23:20 +0900

    spring cloud gateway - 프로젝트 생성

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,9 +1,11 @@
 package com.example.secondservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/second-service")
 public class SecondServiceController {
 
     @GetMapping("/welcome")
