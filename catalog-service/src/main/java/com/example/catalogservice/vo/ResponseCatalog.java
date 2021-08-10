0d665f2d4948ae57612d35baa805364c3b904945
seongbinko <wnrhd1082@gmail.com>
2021-08-10 15:09:45 +0900

    spring cloud catalog-microservice basic 구현

---
+++ catalog-service/src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
@@ -0,0 +1,13 @@
+package com.example.catalogservice.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseCatalog {
+
+    private String productId;
+    private Integer unitPrice;
+    private Integer totalPrice;
+}
